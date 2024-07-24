2def14ee1f8aae5a08d057519caed181f5b42395
YOURI CHANCRIN <Youri.Chancrin@bordeaux-inp.fr>
2024-07-24 23:41:59 +0200

    First commit

---
+++ src/main/kotlin/com/cludivers/kz_survivor/KzSurvivor.kt
@@ -0,0 +1,14 @@
+package com.cludivers.kz_survivor
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class KzSurvivor : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
